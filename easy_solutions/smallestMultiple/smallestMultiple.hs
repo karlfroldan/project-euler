59c807aafd0f8a6f5d61afbb5b99a0a2e8787681
Karl Roldan <36784362+karlfroldan@users.noreply.github.com>
2020-10-03 20:47:02 +0800

    update

--- easy_solutions/smallestMultiple/smallestMultiple.hs
+++ easy_solutions/smallestMultiple/smallestMultiple.hs
@@ -1,9 +1,21 @@
-divides :: Int -> Int -> Bool 
-a `divides` b = b `mod` a == 0
+exchange :: (Num a, Ord a) => a -> a -> (a, a)
+exchange a b 
+    | a <= b    = (b, a)
+    | otherwise = (a, b)
 
-smallestMultiple :: Int -> Int
-smallestMultiple x = let dividesAll a = all (a `divides`) [1..x]
-                     in head [y | y<-[2..], dividesAll y]
+euclideanGcd :: (Integral a) => (a, a) -> a 
+euclideanGcd (n, 0) = n 
+euclideanGcd (n1, n2) = euclideanGcd $ exchange n2 n 
+    where n = n1 `mod` n2
 
-main :: IO ()
-main = print $ smallestMultiple 10
+myLcm :: (Integral a) => (a, a) -> a 
+myLcm (x, y) = (x * y) `div` euclideanGcd(x, y)
+
+multiLcm :: (Integral a) => a -> a
+multiLcm x = multiLcm' [1..x]
+    where multiLcm' = foldl (\acc x -> myLcm (x, acc)) 1
+
+main = do
+    ln <- getLine
+    let lcm' = (multiLcm . read) ln 
+    print lcm'
